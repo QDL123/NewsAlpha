012be9677bba049fe2f15d7323a35f81a7c138d8
qdl <qdl@umich.edu>
2021-01-18 00:52:49 -0500

    started making changes from endpoint tests

--- controllers/Ctrl.cc
+++ controllers/Ctrl.cc
@@ -1,25 +1,28 @@
 #include "Ctrl.h"
 void Ctrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) {
     auto json = req->getJsonObject();
-    bool success = false;
     if (json) {
         std::cout << (*json)["tick"].asString() << std::endl;
         const Json::Value articles = (*json)["text"];
         for (int i = 0; i < articles.size(); ++i) {
             std::cout << articles[i].asString() << std::endl;
         }
-        success = true;
+        // TODO: Send articles to sen
     } else {
-        std::cout << "FUCK WITH ME" << std::endl;
+        std::cout << "NO JSON was received" << std::endl;
+        Json::Value ret;
+        ret["message"] = "Error: No JSON received.";
+        auto resp = HttpResponse::newHttpJsonResponse(ret);
+        // TODO: Find a way to send a status 400 or other code communicating bad request
+        callback(resp);
     }
 
+    // Perform Price derivative analysis
+    
+
     Json::Value ret;
-    ret["result"] = "ok";
-    if (success) {
-        ret["clay"] = "fuck u";
-    } else {
-        ret["clay"] = "fuck u more bitch";
-    }
+    ret["Did we do it?"] = "We did!";
+
     auto resp = HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
 }
